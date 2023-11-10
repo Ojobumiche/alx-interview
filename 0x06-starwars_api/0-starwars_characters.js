e1fe3d48526dc2423cf0b82dd71b1734ce932a09
Ojobumiche <bumiche@gmail.com>
2023-11-10 05:26:06 -0800

    0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,16 +2,27 @@
 
 const request = require('request');
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
-    if (err) throw err;
-    const actors = JSON.parse(body).characters;
-    exactOrder(actors, 0);
-});
 const exactOrder = (actors, x) => {
-    if (x === actors.length) return;
+    if (!actors || x === actors.length) return; // Check if actors is undefined or if x exceeds its length
     request(actors[x], function (err, res, body) {
         if (err) throw err;
-        console.log(JSON.parse(body).name);
+        try {
+            const actorName = JSON.parse(body).name;
+            console.log(actorName);
+        } catch (error) {
+            console.error("Error parsing actor data:", error.message);
+        }
         exactOrder(actors, x + 1);
     });
 };
+
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+    if (err) throw err;
+    try {
+        const filmData = JSON.parse(body);
+        const actors = filmData.characters;
+        exactOrder(actors, 0);
+    } catch (error) {
+        console.error("Error parsing film data:", error.message);
+    }
+});
